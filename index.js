acb553853accf215ea2f6c74ba02b298229d913f
西口達樹 <tatsuson@nishikoutatsujunoMacBook-Pro.local>
2024-02-24 17:47:05 +0900

    about this

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ const mySecondObj = {
 
 // sayFoo();
 mySecondObj.sayFoo();
+
+function MyObj(id){
+    this.id = id;
+}
+
+MyObj.prototype.printId = function(id){
+    console.log(this.id);
+}
+
+const newInstance = new MyObj(5);
