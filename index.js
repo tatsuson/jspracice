7b07de1e45e701bd88685b522bea85e569918702
西口達樹 <tatsuson@nishikoutatsujunoMacBook-Pro.local>
2024-02-27 23:18:22 +0900

    callback

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 console.log('%c [Javascript]', 'color:red; font-size: 1.5em')
-const arr = [5, 3, 8, 1, 4];
-arr.sort((a, b) => {
-    return b - a; // 降順
-});
-console.log(arr);
 
-arr.sort((a, b) => {
-    return a - b; // 昇順
-});
-console.log(arr);
+function wait(callback, num){
+    setTimeout(() => {
+        console.log(num);
+        callback();
+    }, 100);
+}
+
+wait(() => {
+    console.log('callback function is called');
+}, 0);
